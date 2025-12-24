066aff8e707bca90854ad5fec838221cb1f2079b
Bhupendra Jat <118654888+Bhupendra2jat@users.noreply.github.com>
2025-12-24 15:45:53 +0530

    Update Low Incentory Flag.sql

--- SQL Queries/Low Incentory Flag.sql
+++ SQL Queries/Low Incentory Flag.sql
@@ -1,6 +1,5 @@
-SELECT date, store_id, product_id, inveentory_level
-FROM inventory_forecasting
-ORDER BY date;
+avg_units_sold - AVG(units_sold) over last 7 days
+low_inventory_flag = inventory_level < avg_units_sold
 
 
 -- KPI meaning: Current inventory < last 7-day avg demand
